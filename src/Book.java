a0b7ffa563cc3c9c0e94eecb55abf9b64d805b2d
Evstratyev <orin1111@mail.ru>
2017-10-19 22:44:09 +0300

    1

--- src/Book.java
+++ src/Book.java
@@ -6,9 +6,8 @@ import java.util.Scanner;
 public class Book {
     public static void main(String[] args) {
 
-        Contacts[] phoneBook = new Contacts[5];
+        ContactList newContactList = new ContactList();
 
-        int arrnamecount = 0;
         while (true) {
 
             System.out.println("Menu: " + "\n 1 - Add contact" + "\n 2 - Show all" + "\n 3 - delete contact" + "\n 4 - Exit");
@@ -20,37 +19,51 @@ public class Book {
 
             if (option == 1) {
                 System.out.print("Enter name: ");
-                phoneBook[arrnamecount] = new Contacts();
-
-                phoneBook[arrnamecount].setName(sc.next());
-
+                Contacts newContact = new Contacts();
+                newContact.setName(sc.next());
                 System.out.print("Enter phone: ");
-                phoneBook[arrnamecount].setPhone(sc.next());
-
+                newContact.setPhone(sc.next());
                 System.out.print("Enter e-mail: ");
-                phoneBook[arrnamecount].setEmail(sc.next());
+                newContact.setEmail(sc.next());
 
-                arrnamecount++;
+                newContactList.add(newContact);
 
-            } else if (option == 2) {
-                for (int i = 0; i < phoneBook.length; i++) {
-                    if (phoneBook[i] != null) {
-                        System.out.println(phoneBook[i].toString());
-                    }
+            }
+            else if (option == 2) {
+                for (int i = 0; i < newContactList.size(); i++) {
+                    System.out.println(newContactList.get(i).toString());
+                    System.out.println("Contact List size: " +newContactList.size());
                 }
+            }
 
-
-            } else if (option == 3) {
-                System.out.print("Enter contact to delete: ");
-                String contactDelete = sc.next();
-                for (int i = 0; i < phoneBook.length; i++) {
-                    if (phoneBook[i].getName().equals(contactDelete)) {
-                        phoneBook[i] = null;
-                        break;
-                    }
-                }
+            else if (option == 3) {
+                System.out.print("Delete contact number: ");
+                int removeTemp = sc.nextInt();
+                newContactList.remove(removeTemp);
             } else break;
         }
     }
 }
+
+//            } else if (option == 2) {
+//                for (int i = 0; i < phoneBook.length; i++) {
+//                    if (phoneBook[i] != null) {
+//                        System.out.println(phoneBook[i].toString());
+//                    }
+//                }
+//
+//
+//            } else if (option == 3) {
+//                System.out.print("Enter contact to delete: ");
+//                String contactDelete = sc.next();
+//                for (int i = 0; i < phoneBook.length; i++) {
+//                    if (phoneBook[i].getName().equals(contactDelete)) {
+//                        phoneBook[i] = null;
+//                        break;
+//                    }
+//                }
+//            } else break;
+//        }
+//    }
+//}
 
---
+++ src/ContactList.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Евген on 16.10.2017.
+ */
+public class ContactList {
+
+   private Contacts[] arrayContactList;
+
+    public void add (Contacts contact){
+
+        if (arrayContactList == null){
+        arrayContactList = new Contacts[1];
+        arrayContactList[0] = contact;
+        } else {
+            Contacts[] temp = arrayContactList;
+            arrayContactList = new Contacts[arrayContactList.length + 1];
+            for (int i = 0; i < arrayContactList.length-1; i++){
+                arrayContactList[i] = temp[i];
+                arrayContactList[arrayContactList.length-1] = contact;
+            }
+        }
+    }
+
+    public void remove (int removeContactIndex){
+
+        Contacts[] temp = arrayContactList;
+        arrayContactList = new Contacts[arrayContactList.length-1];
+        for (int i = 0; i < removeContactIndex-1; i++){
+            arrayContactList[i] = temp[i];
+            }
+        for (int j = removeContactIndex-1; j < arrayContactList.length; j++){
+            arrayContactList[j] = temp[j+1];
+        }
+    }
+
+    public int size (){
+        int size = arrayContactList.length;
+        return size;
+    }
+
+    public Contacts get (int index){
+        return arrayContactList[index];
+    }
+}
+
